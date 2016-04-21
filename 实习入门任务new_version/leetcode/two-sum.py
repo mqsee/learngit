9869027de027a3b1ac511d00c8dcafa553f3bc7a
moqi <moqi@wecash.net>
2016-04-21 11:25:04 +0800

    delete

--- 实习入门任务new_version/leetcode/two-sum.py
+++
@@ -1,8 +0,0 @@
-class Solution(object):
-    def twoSum(self, nums, target):
-        mydict = {}
-        for pos,x in enumerate(nums):
-            if(mydict.get(target-x,None) == None):
-                mydict[x]=pos
-            else:
-                return mydict[target-x],pos
--- 实习入门任务old_version/leetcode/two-sum.py
+++
@@ -1,9 +0,0 @@
-class Solution(object):
-    def twoSum(self, nums, target):
-        dict = {}
-        l = len(nums)
-        for i in range(l):
-            if(dict.get(target-nums[i],None) == None):
-                dict[nums[i]]=i
-            else:
-                return dict[target-nums[i]],i
